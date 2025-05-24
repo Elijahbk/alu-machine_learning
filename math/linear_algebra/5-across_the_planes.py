2fd24541846c470cc1f81a6de6f920f35b20f04b
keb03 <134435252+Elijahbk@users.noreply.github.com>
2025-05-24 22:47:18 +0200

    Update 5-across_the_planes.py

--- math/linear_algebra/5-across_the_planes.py
+++ math/linear_algebra/5-across_the_planes.py
@@ -1,8 +1,6 @@
 #!/usr/bin/env python3
 def add_matrices2D(mat1, mat2):
-    """
-    Adds two 2D matrices element-wise
-    """
-    if len(mat1) != len(mat2) or any(len(r1) != len(r2) for r1, r2 in zip(mat1, mat2)):
+    """Add two 2D matrices element-wise. Returns None if shapes don't match."""
+    if [len(r) for r in mat1] != [len(r) for r in mat2]:
         return None
-    return [[a + b for a, b in zip(row1, row2)] for row1, row2 in zip(mat1, mat2)]
+    return [[a+b for a,b in zip(r1,r2)] for r1,r2 in zip(mat1,mat2)]
