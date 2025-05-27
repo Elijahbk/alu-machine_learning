24cc051d441c9f5796253b18e25a948dc6c2086e
keb03 <134435252+Elijahbk@users.noreply.github.com>
2025-05-27 15:50:25 +0200

    Update 12-bracin_the_elements.py

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
-import numpy as np
 def np_elementwise(mat1, mat2):
-    add = mat1 + mat2
-    sub = mat1 - mat2
-    mul = mat1 * mat2
-    div = mat1 / mat2
+    # Determine if mat2 is a scalar (int or float)
+    if isinstance(mat2, (int, float)):
+        # Perform operations with scalar
+        add = [[elem + mat2 for elem in row] for row in mat1]
+        sub = [[elem - mat2 for elem in row] for row in mat1]
+        mul = [[elem * mat2 for elem in row] for row in mat1]
+        div = [[elem / mat2 for elem in row] for row in mat1]
+    else:
+        # Perform element-wise operations between two matrices
+        add = [[mat1[i][j] + mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
+        sub = [[mat1[i][j] - mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
+        mul = [[mat1[i][j] * mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
+        div = [[mat1[i][j] / mat2[i][j] for j in range(len(mat1[0]))] for i in range(len(mat1))]
     return (add, sub, mul, div)
