fc37ad13b351f2d75e1a266f9644ba20f0c3a357
keb03 <134435252+Elijahbk@users.noreply.github.com>
2025-05-27 15:47:14 +0200

    Update 12-bracin_the_elements.py

--- math/linear_algebra/12-bracin_the_elements.py
+++ math/linear_algebra/12-bracin_the_elements.py
@@ -9,30 +9,9 @@ on matrices using NumPy's vectorized operations.
 
 import numpy as np
 
-
 def np_elementwise(mat1, mat2):
-    """
-    Perform element-wise operations on two matrices using NumPy.
-
-    Args:
-        mat1: First matrix (numpy.ndarray)
-        mat2: Second matrix (numpy.ndarray or scalar)
-
-    Returns:
-        tuple: Contains four numpy.ndarrays resulting from:
-              (addition, subtraction, multiplication, division)
-
-    Note:
-        Uses NumPy's broadcasting rules for operations between
-        arrays of different shapes or between arrays and scalars.
-
-    Example:
-        >>> mat1 = np.array([[11, 22], [33, 44]])
-        >>> mat2 = np.array([[1, 2], [3, 4]])
-        >>> np_elementwise(mat1, mat2)
-        (array([[12, 24], [36, 48]]),
-         array([[10, 20], [30, 40]]),
-         array([[ 11,  44], [ 99, 176]]),
-         array([[11. , 11. ], [11. , 11. ]]))
-    """
-    return (mat1 + mat2, mat1 - mat2, mat1 * mat2, mat1 / mat2)
+    add = mat1 + mat2
+    sub = mat1 - mat2
+    mul = mat1 * mat2
+    div = mat1 / mat2
+    return (add, sub, mul, div)
