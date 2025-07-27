dc9b20174a02369d8c4212a7521139d34745872e
keb03 <134435252+Elijahbk@users.noreply.github.com>
2025-07-27 18:57:18 +0200

    Update 13-cats_got_your_tongue.py

--- math/linear_algebra/13-cats_got_your_tongue.py
+++ math/linear_algebra/13-cats_got_your_tongue.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
+"""Demonstrate array concatenation using numpy"""
 import numpy as np
 
 
 def np_cat(mat1, mat2, axis=0):
-    return np.concatenate((mat1, mat2), axis=axis)
+    """Concatenate mat1 and mat2 along the specified axis"""
+    return np.concatenate([mat1, mat2], axis=axis)
