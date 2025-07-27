7e96723cad453c4a54cbe8e024c0a922291b2551
keb03 <134435252+Elijahbk@users.noreply.github.com>
2025-07-27 18:54:33 +0200

    Updating 7-gettin_cozy.py

--- math/linear_algebra/7-gettin_cozy.py
+++ math/linear_algebra/7-gettin_cozy.py
@@ -1,12 +1,24 @@
 #!/usr/bin/env python3
+"""
+concatenation is alike adding to a vector,
+many vectors becoming one
+"""
+
+
 def cat_matrices2D(mat1, mat2, axis=0):
-    """Concatenates two 2D matrices along a given axis"""
+
+    """
+    we do this ay addying two vectors
+    together, with the addtion sign
+    """
     if axis == 0:
-        if len(mat1[0]) != len(mat2[0]):
+        if len(mat1) and len(mat2) and len(mat1[0]) != len(mat2[0]):
             return None
-        return mat1 + mat2
-    elif axis == 1:
+        return [row[:] for row in mat1] + [row[:] for row in mat2]
+
+    if axis == 1:
         if len(mat1) != len(mat2):
             return None
-        return [row1 + row2 for row1, row2 in zip(mat1, mat2)]
+        return [row1[:] + row2[:] for row1, row2 in zip(mat1, mat2)]
+
     return None
